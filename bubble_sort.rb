05562647d00125c7e4953349d4865c6acbab27cd
Dan <dbateman0319@starkstate.net>
2017-07-16 12:09:34 -0500

    Created bubble_sort_by method rough draft

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -10,10 +10,6 @@ def bubble_sort(array)
 				temp = array[i - 1]
 				array[i - 1] = array[i]
 				array[i] = temp
-				
-			
-				
-				
 			end
 			i += 1
 
@@ -23,6 +19,35 @@ def bubble_sort(array)
 	end
 	return array
 end
+
+def bubble_sort_by(array)
+	length = array.length
+	i = 1
+	temp = 0
+	swap = 0
+	x = 0
+	for x in x..length - 1 do
+		while i < length
+			if array[i - 1] > array[i]
+				temp = array[i - 1]
+				array[i - 1] = array[i]
+				array[i] = temp
+			end
+			i += 1
+
+		end
+		i = 1
+		
+	end
+	return array
+end
+
+bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+	left.length - right.length
+end
+
+
+
 
 my_array = [6, 2, 5, 4, 12, 53, 19, 4, 3, 8, 13, 1, 17]
 
